621167ffbd28b312a029c416a5137d7d39a50066
Jason M Duguay <duguay.jason@gmail.com>
2020-02-29 08:50:35 -0500

    Add files via upload

---
+++ slurm/gosman.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#SBATCH --account=def-lacey
+#SBATCH --nodes=2
+#SBATCH --ntasks-per-node=40
+#SBATCH --mem=0
+#SBATCH --time=0-24:00
+#SBATCH --output=gosman.out
+mpirun -np 80  twoPhaseEulerFoam -parallel
