ff83342d5958de29bd11df90bd1901314d3ce18b
Fishified <duguay.jason@gmail.com>
2020-03-16 17:50:06 -0400

    add decomposePar.192

--- slurm/slurmwallLift.sh
+++ slurm/slurmwallLift.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --account=def-lacey
-#SBATCH --nodes=2
-#SBATCH --ntasks-per-node=40
+#SBATCH --nodes=24
+#SBATCH --ntasks-per-node=8
 #SBATCH --mem=0
 #SBATCH --time=0-24:00
-#SBATCH --output=gosman.out
-mpirun -np 80  twoPhaseEulerFoam -parallel
+#SBATCH --output=wallLift.out
+mpirun -np 192 twoPhaseEulerFoam -parallel
