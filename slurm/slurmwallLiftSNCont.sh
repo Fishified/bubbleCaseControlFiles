1e8caf68fe048b181e5f6fdeee6cdbf3c6214a40
Fishified <duguay.jason@gmail.com>
2020-03-25 12:55:20 -0400

    add wallLiftSNCont

---
+++ slurm/slurmwallLiftSNCont.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#SBATCH --account=def-lacey
+#SBATCH --nodes=24
+#SBATCH --ntasks-per-node=8
+#SBATCH --mem=0
+#SBATCH --time=0-24:00
+#SBATCH --output=wallLiftSNCont.out
+mpirun -np 192 twoPhaseEulerFoam -parallel
